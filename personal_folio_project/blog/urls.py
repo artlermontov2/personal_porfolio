0c1953dcfe3e5508219a56f3a55629509c83c22d
Art <artist.bryansk@gmail.com>
2022-04-24 10:47:23 +0300

    Создал urls файлы для приложение и пространство имён для них

---
+++ personal_folio_project/blog/urls.py
@@ -0,0 +1,5 @@
+from django import path
+from . import views
+from personal_folio_project import blog
+
+app_name = blog
---
+++ personal_folio_project/portfolio/urls.py
@@ -0,0 +1,5 @@
+from django import path
+from . import views
+from personal_folio_project import portfolio
+
+app_name = portfolio
