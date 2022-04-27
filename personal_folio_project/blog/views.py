7917ed1270982e0610a5d369cff872030c0167b2
Art <artist.bryansk@gmail.com>
2022-04-27 10:25:56 +0300

    создал папку ststic для blog, detail.html, функцию detail

--- personal_folio_project/blog/views.py
+++ personal_folio_project/blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Blogs
 
 
 def all_blogs(request):
-    blogs = Blogs.objects.order_by('-date')[:5]
+    blogs = Blogs.objects.order_by('-date')[:5] 
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+
+def detail(request, blog_id):
+    blog = get_object_or_404(Blogs, pk=blog_id)  # Пытается найти объект под нужным номером, и если не находит, но выбаст ошибку 404
+    return render(request, 'blog/detail.html', {'blog': blog})
